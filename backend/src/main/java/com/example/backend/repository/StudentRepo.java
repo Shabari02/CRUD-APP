a06387382d54548f9eb2083c23fed1e9eab2f658
Shabari02 <lkshabarinathan02@gmail.com>
2023-07-26 16:37:41 +0530

    connected with mysql

--- backend/src/main/java/com/example/backend/repository/StudentRepo.java
+++ backend/src/main/java/com/example/backend/repository/StudentRepo.java
@@ -2,9 +2,6 @@ package com.example.backend.repository;
 
 import com.example.backend.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
 public interface StudentRepo extends JpaRepository<Student,Integer> {
 
 }
